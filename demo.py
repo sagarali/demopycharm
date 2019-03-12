6327664b82f5d091946df86fa5fdcfd21f5b5e63
Shahid <sagar838@yahoo.com>
2019-03-13 00:23:07 +0530

    select query for mysql database

--- demo.py
+++ demo.py
@@ -1,1 +1,8 @@
 print("hello")
+import mysql.connector
+mydb = mysql.connector.connect(host="localhost",user="root",passwd="root",database="test")
+mycursor=mydb.cursor()
+mycursor.execute("select * from bang;")
+result =  mycursor.fetchall()
+for i in result:
+    print(i)
